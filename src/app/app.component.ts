38acd38ea92cf2d224102982dcf928a58cef375e
fatima236 <bouayadi.fatimazahra23@ump.ac.ma>
2025-03-10 11:15:41 +0000

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; // Importez CommonModule
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [
+   
+    CommonModule 
+  ],
   encapsulation: ViewEncapsulation.None // Désactive l'encapsulation
 })
 export class AppComponent {
@@ -14,14 +18,17 @@ title = 'mon-portfolio-fati'
  
 
   // Basculer le menu mobile
-  menuValue:boolean=false;
- menu_icon :string ='bi bi-list';
- openMenu(){
-    this.menuValue =! this.menuValue ;
-    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
-  }
-   closeMenu() {
-    this.menuValue = false;
-    this.menu_icon = 'bi bi-list';
-  }
+ 
+    menuValue: boolean = false;
+    menu_icon: string = 'bi bi-list';
+  
+    openMenu() {
+      this.menuValue = !this.menuValue;
+      this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+    }
+  
+    closeMenu() {
+      this.menuValue = false;
+      this.menu_icon = 'bi bi-list';
+    }
 }
